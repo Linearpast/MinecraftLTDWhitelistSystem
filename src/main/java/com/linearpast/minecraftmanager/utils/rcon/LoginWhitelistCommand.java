47590bd134c739ad3a8482d7e3aff379e05338a5
LostInLinearPast <1283411677@qq.com>
2025-08-02 15:14:24 +0800

    add add-command

--- src/main/java/com/linearpast/minecraftmanager/utils/rcon/LoginWhitelistCommand.java
+++ src/main/java/com/linearpast/minecraftmanager/utils/rcon/LoginWhitelistCommand.java
@@ -2,9 +2,7 @@ package com.linearpast.minecraftmanager.utils.rcon;
 
 import com.linearpast.minecraftmanager.utils.WhitelistTarget;
 import com.linearpast.minecraftmanager.utils.config.SelfConfig;
-import io.graversen.minecraft.rcon.commands.base.BaseTargetedCommand;
 import io.graversen.minecraft.rcon.commands.base.ICommand;
-import io.graversen.minecraft.rcon.util.Target;
 import io.graversen.minecraft.rcon.util.WhiteListModes;
 import lombok.Getter;
 import org.apache.commons.text.StringSubstitutor;
@@ -13,22 +11,18 @@ import java.util.Map;
 import java.util.Objects;
 
 @Getter
-public class LoginWhitelistCommand implements ICommand {
-	private final WhiteListModes whiteListMode;
-	private final WhitelistTarget whitelistTarget;
-
+public record LoginWhitelistCommand(WhitelistTarget whitelistTarget, WhiteListModes whiteListMode) implements ICommand {
 	public LoginWhitelistCommand(WhitelistTarget whitelistTarget, WhiteListModes whiteListMode) {
 		this.whitelistTarget = whitelistTarget;
 		this.whiteListMode = Objects.requireNonNull(whiteListMode);
 	}
 
 	public String command() {
-		return switch (this.getWhiteListMode()) {
-			case ADD ->
-					StringSubstitutor.replace(SelfConfig.addCommand + " ${name} ${uuid}", Map.of(
-							"name", this.getWhitelistTarget().name(),
-							"uuid", this.getWhitelistTarget().uuid())
-					);
+		return switch (this.whiteListMode()) {
+			case ADD -> StringSubstitutor.replace(SelfConfig.addCommand + " ${name} ${uuid}", Map.of(
+					"name", this.whitelistTarget().name(),
+					"uuid", this.whitelistTarget().uuid())
+			);
 			case REMOVE, LIST, OFF, ON, RELOAD -> "";
 		};
 	}
